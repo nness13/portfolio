4b50c0dd2d504db9f1d88d31c6e2538bb0e10bbb
nness13 <myanest@gmail.com>
2023-09-05 13:20:09 +0300

    Deploy fix

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
-	assetPrefix: '/portfolio'
+	assetPrefix: '/portfolio',
+	publicRuntimeConfig: {
+		basePath: "/portfolio",
+	},
+	basePath: "/portfolio",
 }
 
 module.exports = nextConfig
